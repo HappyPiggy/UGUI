192d9eba6bad73e3ebf13d3c82af0b370b514939
Pigggger <296056428@qq.com>
2016-08-02 17:18:59 +0800

    little game

---
+++ LittleGame/Assets/Scrips/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 10;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+	}
+
+    public void ChangeSpeed(float newSpeed) {
+
+        this.speed = newSpeed;
+    }
+    
+}
