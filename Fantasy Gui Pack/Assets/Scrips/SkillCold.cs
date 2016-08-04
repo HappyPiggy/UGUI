0a69e36ba2ee0842995b3e99755402885bedc1d5
Pigggger <296056428@qq.com>
2016-08-04 19:06:57 +0800

    Add a new Scene

---
+++ Fantasy Gui Pack/Assets/Scrips/SkillCold.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SkillCold : MonoBehaviour
+{
+
+    public float coldTime = 2;
+
+    private float Timer = 0;
+    private bool isStart = false;
+    private Image bgImage;
+
+
+    private void Start()
+    {
+        bgImage = transform.Find("bg1").GetComponent<Image>();
+    }
+
+
+    private void Update()
+    {
+        if (isStart)
+        {
+            Timer += Time.deltaTime;
+            bgImage.fillAmount = (coldTime - Timer)/coldTime;
+
+            if (Timer>=coldTime)
+            {
+                bgImage.fillAmount = 0;
+                Timer = 0;
+                isStart = false;
+            }
+        }
+    }
+
+    public void EnterCold()
+    {
+        isStart = true;
+    }
+}
